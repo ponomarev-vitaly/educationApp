3a5e82b1387a8ff66eae7b2dafd748b97f19b8b6
ponomarev-vitaly <vp211082@gmail.com>
2023-04-28 02:12:17 +0300

    I uncommented the piece of code with the establishing connection with the DB.

--- src/Main.java
+++ src/Main.java
@@ -19,41 +19,45 @@ public class Main {
     private static boolean isDelete = true;
 
     public static void main(String[] args) {
-//        /**
-//         * 1. load and register the driver/connector;
-//         * 2. perform the connection (Location of DB, Username, Password) (double click connector);
-//         * 3. prepare the query (open and type query);
-//         * 4. execute the query (Execute the query);
-//         * 5. perform the logic based on the query (check the given table);
-//         * 6. close the connection (close the workbench);
-//         */
-//
-//        try {
-//            String url = "jdbc:mysql://localhost:3306/edu_pract"; // Java data base connection.
-//            String username = "root";
-//            String password = "volgogradskaya3-60";
-//            Connection db = DriverManager.getConnection(url, username, password);
-//
-//            Statement st = db.createStatement();
-//
-//            ResultSet rs = st.executeQuery("Select * from student");
-//
-//
-//            while (rs.next()) {
-//                // System.out.println(rs.getInt(2));
-//                System.out.println(rs.getInt("student_ID") + "\t\t" +
-//                                   rs.getString("fName") + "\t\t" +
-//                                   rs.getString("lName") + "\t\t" +
-//                                   rs.getString("gender") + "\t\t" +
-//                                   rs.getString("dob"));
-//            }
-//
-//            // System.out.println("We are all good!");
-//            db.close();
-//
-//        } catch (SQLException e) {
-//            // System.out.println("We have a DB error!");;
-//        }
+        /**
+         * 1. load and register the driver/connector;
+         * 2. perform the connection (Location of DB, Username, Password) (double click connector);
+         * 3. prepare the query (open and type query);
+         * 4. execute the query (Execute the query);
+         * 5. perform the logic based on the query (check the given table);
+         * 6. close the connection (close the workbench);
+         */
+
+        try {
+            String url = "jdbc:mysql://localhost:3306/edu_pract"; // Java data base connection.
+            String username = "root";
+            String password = "volgogradskaya3-60";
+            Connection db = DriverManager.getConnection(url, username, password);
+
+            Statement st = db.createStatement();
+
+            // Insert data in the student table.
+            st.execute("INSERT INTO student values (12, 'Mustafa', 'Onat', 'M', '1999-06-10')");
+            ResultSet rs = st.executeQuery("Select * from student"); // Make a query.
+
+            while (rs.next()) {
+                // System.out.println(rs.getInt(2));
+                System.out.println(rs.getInt("student_ID") + "\t\t" +
+                                   rs.getString("fName") + "\t\t" +
+                                   rs.getString("lName") + "\t\t" +
+                                   rs.getString("gender") + "\t\t" +
+                                   rs.getString("dob"));
+            }
+
+            // System.out.println("We are all good!");
+            db.close(); // Close DB connection.
+
+        } catch (SQLException e) {
+            // System.out.println("We have a DB error!");;
+        }
+
+        System.out.println("<-------------------------------------------------------->");
+
         while(input.compareTo("P") != 0){
             System.out.println("\n\n");
             if(mainMenu){
