155431c410c6e61855f29213cd1334de496ee26d
ponomarev-vitaly <vp211082@gmail.com>
2023-04-27 05:46:14 +0300

    I added menu, methods to add new student, display list of students, update students, delete students. Further more I will add course menu and TC menu.

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,129 @@
 import java.sql.*;
+import java.util.Locale;
+import java.util.Scanner;
 import java.util.Stack;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    private static Scanner scan = new Scanner(System.in);
+    private static String input = "";
+    /*C-reate, R-ead, U-pdate, D-elete*/
+    private static boolean mainMenu = true;
+    private static boolean stdMenu = true;
+    private static boolean crMenu = true;
+    private static boolean tcMenu = true;
+    private static boolean isNew = true;
+    private static boolean isList = true;
+    private static boolean isUpdate = true;
+    private static boolean isDelete = true;
+
     public static void main(String[] args) {
-        /**
-         * 1. load and register the driver/connector;
-         * 2. perform the connection (Location of DB, Username, Password) (double click connector);
-         * 3. prepare the query (open and type query);
-         * 4. execute the query (Execute the query);
-         * 5. perform the logic based on the query (check the given table);
-         * 6. close the connection (close the workbench);
-         */
-
-        try {
-            String url = "jdbc:mysql://localhost:3306/edu_pract"; // Java data base connection.
-            String username = "root";
-            String password = "volgogradskaya3-60";
-            Connection db = DriverManager.getConnection(url, username, password);
-
-            Statement st = db.createStatement();
-
-            ResultSet rs = st.executeQuery("Select * from student");
-
-
-            while (rs.next()) {
-                // System.out.println(rs.getInt(2));
-                System.out.println(rs.getInt("student_ID") + "\t\t" +
-                                   rs.getString("fName") + "\t\t" +
-                                   rs.getString("lName") + "\t\t" +
-                                   rs.getString("gender") + "\t\t" +
-                                   rs.getString("dob"));
+//        /**
+//         * 1. load and register the driver/connector;
+//         * 2. perform the connection (Location of DB, Username, Password) (double click connector);
+//         * 3. prepare the query (open and type query);
+//         * 4. execute the query (Execute the query);
+//         * 5. perform the logic based on the query (check the given table);
+//         * 6. close the connection (close the workbench);
+//         */
+//
+//        try {
+//            String url = "jdbc:mysql://localhost:3306/edu_pract"; // Java data base connection.
+//            String username = "root";
+//            String password = "volgogradskaya3-60";
+//            Connection db = DriverManager.getConnection(url, username, password);
+//
+//            Statement st = db.createStatement();
+//
+//            ResultSet rs = st.executeQuery("Select * from student");
+//
+//
+//            while (rs.next()) {
+//                // System.out.println(rs.getInt(2));
+//                System.out.println(rs.getInt("student_ID") + "\t\t" +
+//                                   rs.getString("fName") + "\t\t" +
+//                                   rs.getString("lName") + "\t\t" +
+//                                   rs.getString("gender") + "\t\t" +
+//                                   rs.getString("dob"));
+//            }
+//
+//            // System.out.println("We are all good!");
+//            db.close();
+//
+//        } catch (SQLException e) {
+//            // System.out.println("We have a DB error!");;
+//        }
+        while(input.compareTo("P") != 0){
+            System.out.println("\n\n");
+            if(mainMenu){
+                displayMainMenu();
+            } else if (stdMenu){
+                displayStdMenu();
+            } else if (crMenu){
+                displayCourseMenu();
+            } else if(tcMenu){
+                displayTCMenu();
             }
-
-            // System.out.println("We are all good!");
-            db.close();
-
-        } catch (SQLException e) {
-            // System.out.println("We have a DB error!");;
         }
+    }
+
+    private static void displayMainMenu(){
+        System.out.println("------------------EDUCATION APPLICATION------------------");
+        System.out.println("------------------       MAIN MENU     ------------------");
+        System.out.println("(S)tudents");
+        System.out.println("(C)ourses");
+        System.out.println("(T)aken courses");
+        System.out.println("(P)ower off");
+
+        input = scan.next().toUpperCase().substring(0,1);
+        if(input.compareTo("S") == 0){
+            mainMenu = false;
+            stdMenu = true;
+        } else if(input.compareTo("C") == 0){
+            mainMenu = false;
+            crMenu = true;
+        } else if(input.compareTo("T") == 0) {
+            mainMenu = false;
+            tcMenu = true;
+        }
+    }
+
+    private static void displayStdMenu(){
+        System.out.println("------------------EDUCATION APPLICATION------------------");
+        System.out.println("------------------     STUDENT MENU    ------------------");
+        System.out.println("(A)DD NEW STUDENT");
+        System.out.println("(L)IST OF STUDENTS");
+        System.out.println("(U)PDATE OF STUDENTS");
+        System.out.println("(D)ELETE A STUDENT DETAIL");
+        System.out.println("(M)ain menu");
+        System.out.println("(P)ower off");
+        System.out.print("-------------------------------------------------------->");
+
+        input = scan.next().toUpperCase().substring(0,1);
+        if(input.compareTo("A") == 0){
+            System.out.println("Here we will add a new student");
+        } else if(input.compareTo("L") == 0){
+            System.out.println("Here we will display the list of students");
+        } else if(input.compareTo("U") == 0) {
+            System.out.println("Here we will update a student");
+        } else if(input.compareTo("D") == 0) {
+            System.out.println("Here we will delete a student");
+        } else if(input.compareTo("M") == 0){
+            stdMenu = false;
+            mainMenu = true;
+        }
+    }
+
+    private static void displayCourseMenu(){
+        System.out.println("UNDER DEVELOPMENT");
+        crMenu = false;
+        mainMenu = true;
+    }
+
+    private static void displayTCMenu(){
+        System.out.println("UNDER DEVELOPMENT");
+        tcMenu = false;
+        mainMenu = true;
     }
 }
