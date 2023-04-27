846651f30c85d2015092a16c2f13b0f2e2aa3e16
ponomarev-vitaly <vp211082@gmail.com>
2023-04-27 03:53:50 +0300

    I added url, username, password, Connection db.

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        /**
+         * 1. load and register the driver/connector;
+         * 2. perform the connection (Location of DB, Username, Password);
+         * 3. prepare the query;
+         * 4. execute the query;
+         * 5. perform the logic based on the query;
+         * 6. close the connection;
+         */
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        String url = "jdbs:mysql://localhost:3306/edu_pract"; // Java data base connection.
+        String username = "root";
+        String password = "volgogradskaya3-60";
+        Connection db = DriverManager.getConnection(url, username, password);
     }
 }
